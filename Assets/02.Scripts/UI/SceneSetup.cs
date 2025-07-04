4dbd3e8f279e99b3dad30b6007ae3bdbe48e4257
YJSon91 <sko9020@naver.com>
2025-07-05 00:18:18 +0900

    refactor(UI) : 스타트 판넬 리팩토링
    IntroUIFlow,SceneSetup 삭제하고 StartPanelUI로 통합했습니다. 이제 게임씬으로 넘어가도 UI가 정상작동합니다.

--- Assets/02.Scripts/UI/SceneSetup.cs
+++ Assets/02.Scripts/UI/SceneSetup.cs
@@ -7,10 +7,10 @@ public class SceneSetup : MonoBehaviour
     void Awake()
     {
         // 이 오브젝트에 붙어있는 IntroUIFlow 컴포넌트를 찾습니다.
-        IntroUIFlow introFlow = GetComponent<IntroUIFlow>();
+        StartPanelUI introFlow = GetComponentInChildren<StartPanelUI>();
 
         // 만약 IntroUIFlow가 있다면 (그리고 비활성화 상태라면)
-        if (introFlow != null)
+        if (StartPanelUI != null)
         {
             // 스크립트를 활성화시켜 일을 시작하도록 합니다.
             introFlow.enabled = true;
