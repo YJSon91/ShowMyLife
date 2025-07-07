272a76b431382128523630deb5d42aa18c84a024
YJSon91 <sko9020@naver.com>
2025-07-07 14:44:16 +0900

    fix(Sound/UI) : 사운드리소스 추가, 에디터 게임종료 기능 추가

--- Assets/02.Scripts/Manager/GameManager.cs
+++ Assets/02.Scripts/Manager/GameManager.cs
@@ -202,7 +202,7 @@ public class GameManager : MonoBehaviour
                 break;
 
             case GameState.MainMenu:
-                SoundManager?.PlayBGM(BgmType.Lobby);
+                SoundManager?.PlayBGM(BgmType.Main);
                 break;
 
             case GameState.LevelClear:
@@ -212,23 +212,8 @@ public class GameManager : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None; // 커서 잠금 해제
                 Cursor.visible = true;
                 break;
-        }
-        // 새로운 게임 상태에 맞는 배경음악을 재생합니다.
-        if (SoundManager != null)
-        {
-            switch (newState)
-            {
-                case GameState.MainMenu:
-                    SoundManager.PlayBGM(BgmType.Lobby);
-                    break;
-                case GameState.Playing:
-                    SoundManager.PlayBGM(BgmType.Main);
-                    break;
-                case GameState.LevelClear:
-                    SoundManager.PlayBGM(BgmType.GameOver); 
-                    break;
-            }
-        }
+        }       
+        
         // 씬 로딩 로직
         if (newState == GameState.Playing)
         {
