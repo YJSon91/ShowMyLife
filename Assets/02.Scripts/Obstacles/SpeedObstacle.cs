f82fbdb595c59c9894b836f20e60bb9c53fa187f
jochn96 <whtjdcks55@naver.com>
2025-08-08 12:49:40 +0900

    chore : 4-2-2 배치 수정

--- Assets/02.Scripts/Obstacles/SpeedObstacle.cs
+++ Assets/02.Scripts/Obstacles/SpeedObstacle.cs
@@ -50,4 +50,17 @@ public class SpeedObstacle : BaseObstacle
         if (controller != null)
             controller.ExitSlowZone();
     }
+
+     private void OnDrawGizmos()
+    {
+        Collider col = GetComponent<Collider>();
+        if (col is BoxCollider box && box.isTrigger)
+        {
+            Gizmos.color = new Color(0.45f, 0.45f, 0.45f, 0.45f);
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawCube(box.center, box.size);
+            Gizmos.color = new Color(0.45f, 0.45f, 0.45f, 1f);
+            Gizmos.DrawWireCube(box.center, box.size);
+        }
+    }
 }
